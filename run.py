7f586dcfbb61b88449ef3e681d15f9a0a21ac6bf
Karthik-Pilot <karthickjay@gmail.com>
2026-01-31 17:05:01 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from app import create_app
+
 app = create_app()
 
 if __name__ == "__main__":
@@ -7,4 +8,3 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
     )
-
